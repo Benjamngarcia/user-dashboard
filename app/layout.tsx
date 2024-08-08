771b317d144312b6db6c3f4ef920eb850bafbed4
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2024-08-07 21:33:19 -0600

    mod: update app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"]
+});
+
 
 export const metadata: Metadata = {
   title: "Users Dashboard",
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${montserrat.className} bg-gray-300`}>
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-7">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
