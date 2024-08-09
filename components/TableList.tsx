ed6331a58b139efcbd3b1f5d56837c12505383af
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2024-08-08 23:13:13 -0600

    mod: redirect each row to /users/:id

--- components/TableList.tsx
+++ components/TableList.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import {
   IconTrash,
   IconAt,
@@ -13,6 +17,12 @@ interface TableProps {
 }
 
 const UserTable: React.FC<TableProps> = ({ users, openModal }) => {
+  const router = useRouter();
+
+  const handleLink = (uuid: string) => {
+    router.push(`/users/${uuid}`);
+  };
+
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full leading-normal">
@@ -23,24 +33,24 @@ const UserTable: React.FC<TableProps> = ({ users, openModal }) => {
             </th>
             <th className="px-3 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
               <div className="flex items-center gap-1">
-                <IconAt stroke={1} size={20}/> Email
+                <IconAt stroke={1} size={20} /> Email
               </div>
             </th>
             <th className="px-3 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
               <div className="flex items-center gap-1">
-                <IconBlendMode stroke={1} size={20}/>
+                <IconBlendMode stroke={1} size={20} />
                 Gender
               </div>
             </th>
             <th className="px-3 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
               <div className="flex items-center gap-1">
-                <IconMapPin stroke={1} size={20}/>
+                <IconMapPin stroke={1} size={20} />
                 Location
               </div>
             </th>
             <th className="px-3 py-2 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-medium text-gray-600 uppercase tracking-wider">
               <div className="flex items-center gap-1">
-                <IconPlaystationCircle stroke={1} size={20}/>
+                <IconPlaystationCircle stroke={1} size={20} />
                 Actions
               </div>
             </th>
@@ -51,6 +61,7 @@ const UserTable: React.FC<TableProps> = ({ users, openModal }) => {
             <tr
               key={user.login.uuid}
               className="bg-white hover:bg-gray-100 duration-300 ease-in-out"
+              onClick={() => handleLink(user.login.uuid)}
             >
               <td className="px-3 py-3 border-b border-gray-200 text-xs">
                 <div className="flex items-center gap-2">
@@ -62,7 +73,7 @@ const UserTable: React.FC<TableProps> = ({ users, openModal }) => {
                   {user.name.first} {user.name.last}
                 </div>
               </td>
-              <td className="px-3 py-3 border-b border-gray-200 text-xs">
+              <td className="px-3 py-3 border-b border-gray-200 text-xs underline">
                 {user.email}
               </td>
               <td className="px-3 py-3 border-b border-gray-200 text-xs">
@@ -74,7 +85,10 @@ const UserTable: React.FC<TableProps> = ({ users, openModal }) => {
               <td className="px-3 py-3 border-b border-gray-200 text-xs">
                 <button
                   type="button"
-                  onClick={() => openModal(user)}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    openModal(user);
+                  }}
                   className="flex items-center text-red-500 hover:text-white border border-red-500 duration-300 ease-in-out hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-xs px-2 py-1 text-center"
                 >
                   <IconTrash /> Delete
