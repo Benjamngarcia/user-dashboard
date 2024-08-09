ef302606a83b453d189cc803179faa49ac11b730
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2024-08-09 15:46:04 -0600

    fix: ' error on build

--- components/Modals/ConfirmationDelete.tsx
+++ components/Modals/ConfirmationDelete.tsx
@@ -38,7 +38,7 @@ const ConfirmationDelete: React.FC<ConfirmationDeleteProps> = ({
               onClick={onConfirm}
               className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
             >
-              Yes, I'm sure
+              Yes, I am sure
             </button>
             <button
               type="button"
