b223ec57fca0fad6a8df365296708020975267df
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2024-08-09 15:30:24 -0600

    mod: update next config for deploy with alias imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from "path";
+
+const __dirname = path.resolve();
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["@"] = path.resolve(__dirname);
+    }
+
+    config.resolve.alias["@/components"] = path.resolve(
+      __dirname,
+      "components"
+    );
+    config.resolve.alias["@/api"] = path.resolve(__dirname, "api");
+    config.resolve.alias["@/context"] = path.resolve(__dirname, "context");
+    config.resolve.alias["@/helpers"] = path.resolve(__dirname, "helpers");
+
+    return config;
+  },
+};
 
 export default nextConfig;
